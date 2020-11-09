448e2b0c668950399a167ba502603e9bf4c83a71
lxiong515 <58718122+lxiong515@users.noreply.github.com>
2020-11-09 14:06:50 -0600

    removed Class getters/setters
    removed getters/setters for lombok

--- src/main/java/dmacc/beans/HolidayParty.java
+++ src/main/java/dmacc/beans/HolidayParty.java
@@ -16,34 +16,5 @@ public class HolidayParty {
 	public HolidayParty(String eventName, String street, String city, String state) {
 
 	}
-	//getters and setters
-	public String getEventName() {
-		return eventName;
-	}
-	public void setEventName(String eventName) {
-		this.eventName = eventName;
-	}
-	public String getStreet() {
-		return street;
-	}
-	public void setStreet(String street) {
-		this.street = street;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	@Override
-	public String toString() {
-		return "Holiday Party [Event Name=" + eventName + " , street=" + street + " , city=" + city + " , state=" + state + "]";
-	}
-
+	
 }
--- src/main/java/dmacc/beans/Presents.java
+++ src/main/java/dmacc/beans/Presents.java
@@ -36,40 +36,7 @@ public class Presents {
 	public Presents(String name, String present, String relationship) {
 		
 	}
-	//create getters and setters
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPresent() {
-		return present;
-	}
-	public void setPresent(String present) {
-		this.present = present;
-	}
-	public String getRelationship() {
-		return relationship;
-	}
-	public void setRelationship(String relationship) {
-		this.relationship = relationship;
-	}
 	
-	@Override
-	public String toString() {
-		return "Recipient [id = " + id +", name=" + name + ", present=" + present + ", relationship=" + relationship + "]";
-	}
-
-	public void setHolidayParty(HolidayParty h) {
-		// TODO Auto-generated method stub
-		this.holidayParty = holidayParty;
-	}
+	
 
 }
